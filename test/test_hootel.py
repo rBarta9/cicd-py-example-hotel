e8c76ee99c88707b765f979b2faf597266896c82
rBarta9 <raksag@gmail.com>
2026-09-14 20:44:46 +0200

    Fix test file

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -5,57 +5,65 @@ from selenium.webdriver.common.by import By
 import allure
 import pytest
 
+USERS = {
+    'goodUser': {
+        'email': 'hiwasi1765@wisnick.com',
+        'password': 'tesztelek2021'
+    },
+    'badUser': {
+        'email': 'hiwasi1765@wisnick.com',
+        'password': 'incorrect'
+    }
+}
 
 class TestHootel(object):
     def setup_method(self):
         URL = 'http://hotel-v3.progmasters.hu/'
         options = Options()
         options.add_experimental_option("detach", True)
-        # options.add_argument('--start-maximized')
-        options.add_argument('--headless')
         options.add_argument('--guest')
-        options = Options()
-        # options.add_experimental_option("detach", True)
-        prefs = {
-            "https_only_mode_enabled": False,
-            # "https_first_balanced_mode_enabled": False,
-            # "https_first_mode_incognito_enabled": False
-        }
-
-        options.add_experimental_option("prefs", prefs)
-        # browser = webdriver.Chrome(options=options)
-        # browser.maximize_window()
+        options.add_argument('--start-maximized')
+        options.add_argument('--headless')
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(URL)
+        print(self.browser.get_window_size())
         self.browser.set_window_size(1024, 768)
-        time.sleep(1)
-
+        print(self.browser.get_window_size())
 
     def teardown_method(self):
         self.browser.quit()
 
+
     @allure.title("Hootel Login")
     @allure.description("A belépés tesztelése")
-    @allure.severity(allure.severity_level.TRIVIAL)
-    @allure.tag("login")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.tag("login", "Hootel", "happy-path")
     def test_login(self):
         login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
+        # time.sleep(1)
         login_btn.click()
 
         email_input = self.browser.find_element(By.ID, 'email')
-        email_input.send_keys('hiwasi1765@wisnick.com')
+        # email_input.send_keys('hiwasi1765@wisnick.com')
+        email_input.send_keys(USERS['goodUser']['email'])
 
         password_input = self.browser.find_element(By.ID, 'password')
-        password_input.send_keys('tesztelek2021')
+        # password_input.send_keys('tesztelek2021')
+        password_input.send_keys(USERS['goodUser']['password'])
 
         submit_btn = self.browser.find_element(By.NAME, 'submit')
         submit_btn.click()
         time.sleep(1)
 
         logout_btn = self.browser.find_element(By.ID, 'logout-link')
+        allure.dynamic.description(f"email: {USERS['goodUser']['email']}; password: {USERS['goodUser']['password']}")
 
         assert logout_btn.text == "Kilépés"
 
+    @allure.title("Szálláshelyek listázása")
+    @allure.description("Egy oldalon megjelenített szálláshelyek száma.")
+    @allure.severity(allure.severity_level.NORMAL)
+    @allure.tag("hotels", "Hootel", "happy-path")
     def test_hotel_list(self):
         hotel_list_btn = self.browser.find_element(By.XPATH, '//button[@class="btn btn-outline-primary btn-block"]')
         hotel_list_btn.click()
